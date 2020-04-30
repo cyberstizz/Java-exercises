557c81bd97bb71eb8fdae9f6ee66c3b2db4b4e14
Student_GA106 <student_ga106@Student-GA106.local>
2020-04-29 23:04:01 -0400

    added meter converter class

---
+++ MeterConverter.java
@@ -0,0 +1,9 @@
+public class MeterConverter{
+    public static void main(String[] args){
+        
+    };
+
+    public static void convertToMeters(float centimeters){
+        System.out.println("your conversion to meters is:" + (centimeters * 0.0254));
+    };
+};
